8f5b0752dc36621335dec8d5620e13084e74a5d3
Audrey Langlois <azpmaggie@gmail.com>
2021-05-25 00:20:46 -0400

    video du site terminer

--- src/scripts/ComponentFactory.js
+++ src/scripts/ComponentFactory.js
@@ -1,5 +1,6 @@
 import Carousel from './components/Carousel';
 import Form from './components/Form';
+import Video from './components/Video';
 
 export default class ComponentFactory {
   constructor() {
@@ -7,6 +8,7 @@ export default class ComponentFactory {
     this.componentList = {
       Carousel,
       Form,
+      Video,
     };
     this.init();
   }
---
+++ src/scripts/components/Video.js
@@ -0,0 +1,100 @@
+// Ta magie ici!
+/** Composante Video de TimTools */
+export default class Video {
+  /**
+   * Méthode constructeur
+   * @param {HTMLElement} element - Élément HTML sur lequel la composante est instanciée
+   */
+  constructor(element) {
+    this.element = element;
+    this.videoContainer = this.element.querySelector('.js-video');
+    this.poster = this.element.querySelector('.js-poster');
+    this.autoplay = this.poster ? 1 : 0;
+    this.videoId = this.element.dataset.videoId;
+    this.playerReady = false;
+
+    Video.instances.push(this);
+
+    if (this.videoId) {
+      Video.loadScript();
+    } else {
+      console.error('Vous devez specifier un id');
+    }
+  }
+
+  static loadScript() {
+    if (!Video.scriptIsLoading) {
+      Video.scriptIsLoading = true;
+      const script = document.createElement('script');
+
+      script.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(script);
+    }
+  }
+
+  /**
+   * Méthode d'initialisation
+   */
+  init() {
+    this.initPlayer = this.initPlayer.bind(this);
+    if (this.poster) {
+      this.element.addEventListener('click', this.initPlayer);
+    } else {
+      this.initPlayer();
+    }
+  }
+
+  initPlayer(event) {
+    if (event) {
+      this.element.removeEventListener('click', this.initPlayer);
+    }
+    this.player = new YT.Player(this.videoContainer, {
+      height: '100%',
+      width: '100%',
+      videoId: this.videoId,
+      playerVars: { rel: 0, autoplay: this.autoplay },
+      events: {
+        onReady: () => {
+          this.playerReady = true;
+          const observer = new IntersectionObserver(this.watch.bind(this), {
+            rootMargin: '0px 0px 0px 0px',
+          });
+          observer.observe(this.element);
+        },
+        onStateChange: (event) => {
+          if (event.data == YT.PlayerState.PLAYING) {
+            //pause tous les video sauf celui qui joue
+            Video.pauseAll(this);
+          } else if (event.data == YT.PlayerState.ENDED) {
+            this.player.seekTo(0);
+            this.player.pauseVideo();
+          }
+        },
+      },
+    });
+  }
+  watch(entries) {
+    if (this.playerReady && !entries[0].isIntersecting) {
+      this.player.pauseVideo();
+    }
+  }
+
+  static initAll() {
+    document.documentElement.classList.add('is-video-ready');
+    for (let i = 0; i < Video.instances.length; i++) {
+      const instance = Video.instances[i];
+      instance.init();
+    }
+  }
+  static pauseAll(currentInstance) {
+    for (let i = 0; i < Video.instances.length; i++) {
+      const instance = Video.instances[i];
+      if (instance.playerReady && instance !== currentInstance) {
+        instance.player.pauseVideo();
+      }
+    }
+  }
+}
+
+Video.instances = [];
+window.onYouTubeIframeAPIReady = Video.initAll;
